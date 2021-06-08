91e2a596ddb5b8b4570a33bd7b586683ef8a3b54
clapmin <minclap92@gmail.com>
2021-06-09 02:36:41 +0900

    미사용 코드 제거

--- src/main/java/com/kakao/bizplatform/navi/domain/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kakao.bizplatform.navi.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
